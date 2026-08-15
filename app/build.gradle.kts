6ffe4a8ab41e152cac7334f5762c7ba1f380fd29
Shivi <shivanjaligupta10@gmail.com>
2026-08-15 12:28:38 +0530

    updated gitignore, gradle-wrapper.properties and libs.versions.toml.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,17 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    //alias(libs.plugins.kotlin.android)
 }
 
 android {
     namespace = "com.practice.droidapp"
-    compileSdk = 35
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.practice.droidapp"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -27,11 +29,14 @@ android {
         }
     }
     compileOptions {
-      sourceCompatibility = JavaVersion.VERSION_17
-      targetCompatibility = JavaVersion.VERSION_17
+      sourceCompatibility = JavaVersion.VERSION_21
+      targetCompatibility = JavaVersion.VERSION_21
     }
-    kotlinOptions {
-      jvmTarget = "17"
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
     }
 }
 
